175cc3895aece3d5bda1715bf8700ae21d299ada
marcinoles87 <marcinoles87@gmail.com>
2023-12-07 17:14:33 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home'
+import Group from './components/Group';
+import Group2 from './components/Group2';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home></Home>}>
+<>
 
-    </Route>
+
+<ul>
+  <li><Link to="/"> Home</Link></li>
+  <li><Link to="/group1"> Group1</Link></li>
+  <li><Link to="/group2"> Group2</Link></li>
+  
+</ul>
+
+
+    <Routes>
+      <Route path='/' element={<Home></Home>} />
+      <Route path='/group1' element={<Group></Group>} />
+      <Route path='/group2' element={<Group2></Group2>} />
+
+    
     </Routes>
     
+  </>
   );
 }
 
---
+++ src/components/Group2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Group2() {
+  return (
+    <div>Group.2</div>
+  )
+}
+
+export default Group2
