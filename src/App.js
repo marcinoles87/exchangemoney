35ce8081b83f5c217c99e97a42f1e06835e2ccec
marcinoles87 <marcinoles87@gmail.com>
2023-12-07 17:06:50 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home></Home>}>
+
+    </Route>
+    </Routes>
+    
   );
 }
 
